d2d31441925d871ee8a14489ff5ea9ceaf3ed68d
David <davidsolo555@hotmail.com>
2023-06-10 16:09:12 -0700

    cleanup clean file function

--- util/file_util.py
+++ util/file_util.py
@@ -10,34 +10,13 @@ class FileUtil():
     #return a list of file names that start with given prefix
     def filter_files_by_prefix(self, file_list, prefix):
         return list(filter(lambda x: (x.startswith(prefix)), file_list))
-    
-
-    def remove_trailing_digits(file_list, check, clean):
-        for i in range(len(file_list)):
-            if check(file_list[i]) is not None:
-                file_list[i] = clean(file_list[i])
-
 
     def check_for_trailing_digits(file):
         return re.search(r'\d+$', file)
 
     def remove_trailing_digits(file):
         cleaned_file = re.sub(r'\d+$', '', file)
-        return file
-    # def remove_suffix(file_list, suffix):
-    #     #return list(filter(lambda x: (x.endswith(postfix)), file_list))
-    #     #return list(filter(lambda x: postfix), file_list)
-    #     for file in file_list:
-    #         print(suffix)
-    #         if re.search(suffix, file):
-    #             print(suffix)
-    #             #remove digits from end of filename
-    #             end = -1
-    #             while str(file[end]).isdigit():
-    #                 end -= 1
-    #             file = file[:(end+1)]
-    #             print(file)
-    #     return file_list
+        return cleaned_file
 
     if __name__ == "__main__":
 
